d486bde25067ae72cb041c3fb52806e8c9722333
Zeth Thomas <zeththomas89@gmail.com>
2024-02-03 15:20:18 -0500

    Adding places controller

---
+++ controllers/places.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req,res) => {
+    res.send('GET places list')
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -6,13 +6,23 @@ app.get ('/', (req,res) => {
     res.send ('Hello Rest-Rant World!');
 });
 
+app.use('/places', require('./controllers/places'));
+
 app.get('*', (req, res) => {
+    console.log('User requested unknown')
     res.send('<h1>404 Page</h1>')
 })
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
   });
+
+  const router = require('express').Router()
+
+// More code here in a moment
+
+module.exports = router
+
 
 
 
