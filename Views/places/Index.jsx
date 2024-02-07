141811ddc1afad254d19d9678180000cebece084
Zeth Thomas <zeththomas89@gmail.com>
2024-02-07 17:38:02 -0500

    Adding places view with mock data

---
+++ Views/places/Index.jsx
@@ -0,0 +1,28 @@
+const React = require('react');
+const Default = require('../layouts/default');
+
+const Index = (data) => {
+    let placesFormatted = data.places.map((place) => {
+        return (
+            <div key={place.name} className='col-sm-6'>
+                <h2>{place.name}</h2>
+                <p>{place.cuisines}</p>
+                <img src={place.pic} alt={place.name} />
+                <p className='text-center'>
+                    Located in {place.city}, {place.state}
+                </p>
+            </div>
+        );
+    });
+
+    return (
+        <Default>
+            <main>
+                <h1>Places to Rant or Rave About</h1>
+                <div className='row'>{placesFormatted}</div>
+            </main>
+        </Default>
+    );
+};
+
+module.exports = Index;
--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const router = express.Router();
+const app = express.Router();
 
 
-router.get('/', (req,res) => {
-    res.send('GET places list')
-});
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+      res.render('places/index', { places })
 
-module.exports = router;
+  })
+  
+  
+module.exports = app;
