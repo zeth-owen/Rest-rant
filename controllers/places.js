8e0ed7c199dd5e7ffea0d46091d63c5b589c58a3
Zeth Thomas <zeththomas89@gmail.com>
2024-02-13 06:49:12 -0500

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -41,6 +41,21 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
+
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+
   
 
 module.exports = router;
