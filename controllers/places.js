a652c2ee15febda70ce4a019d1da97089c2d0f49
Zeth Thomas <zeththomas89@gmail.com>
2024-02-13 23:26:40 -0500

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -24,6 +24,20 @@ router.get('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
 
 
 router.post('/', (req, res) => {
