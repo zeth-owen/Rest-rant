140de75871801b5a1cc17a548865cb7422c22dcc
Zeth Thomas <zeththomas89@gmail.com>
2024-03-04 00:47:02 -0500

    Schema updated

--- Models/index.js
+++ Models/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI  //'mongodb://localhost:27017/rest-rant';
 
-console.log("MONGO_URI:", process.env.MONGO_URI);
 
 console.log("MONGO_URI:", MONGO_URI);
 
--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const render = require('../render');
 //const places = require('../models/places.js');
-const db = require('../models')
+const db = require('../models/places')
 
 
 
